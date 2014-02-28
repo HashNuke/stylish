394812b79c4fb10761172f40f440918c60e96e45
Akash Manohar J <akash@akash.im>
2014-02-28 15:55:03 +0530

    When updating deps also update submodules

--- src/stylish.erl
+++ src/stylish.erl
@@ -1,5 +1,5 @@
 -module(stylish).
--export([compile/1]).
+-export([compile/1, compile/2]).
 
 -behaviour(application).
 
@@ -18,12 +18,28 @@ stop(_State) ->
 
 
 compile(FilePath) ->
+  compile(FilePath, []).
+
+
+compile(FilePath, Options) ->
+  FormattedOptions = format_options(Options),
   FilePathString = case is_binary(FilePath) of
     true -> binary_to_list(FilePath);
     _ -> FilePath
   end,
 
-  os:cmd( string:join([command_path(), FilePathString], " ") ).
+  CommandParts = [command_path(), FilePathString] ++ [FormattedOptions],
+  os:cmd( string:join(CommandParts, " ") ).
+
+
+
+format_options(Options)->
+  LineNumbers = get_line_numbers_option(Options),
+  SourceMap = get_source_map_option(Options),
+  LoadPaths = get_load_path_options(Options),
+  Output = get_output_path_option(Options),
+
+  string:join([LineNumbers, SourceMap, LoadPaths, Output], " ").
 
 
 command_path() ->
@@ -36,3 +52,43 @@ command_path() ->
                   Path
               end,
     filename:join([PrivDir, "sassc", "bin", "sassc"]).
+
+get_load_path_options(Options)->
+  Paths = proplists:get_value("load_paths", Options, []),
+
+  CleanPathOptions = lists:map(fn(Path)->
+    case is_binary(Path) of
+      true ->
+        binary_to_list("-I " + " " + Path);
+      _ ->
+        "-I " + " " + Path
+    end
+  end, Paths),
+
+  string:join([CleanPathOptions], " ").
+
+
+get_line_numbers_option(Options)->
+  case proplists:get_value("line_numbers", Options) of
+    true -> "-l";
+    _ -> ""
+  end.
+
+
+get_source_map_option(Options)->
+  case proplists:get_value("source_map", Options) of
+    true ->
+      case proplists:get_value("output", Options) of
+        undefined -> "";
+        _ -> "-m"
+      end;
+    _ -> ""
+  end.
+
+
+get_output_path_option(Options)->
+  Output = proplists:get_value("output", Options),
+  case Output of
+    undefined -> "";
+    _ -> Output
+  end.
