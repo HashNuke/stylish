a3f889025ff306e7fdc5c710928bf390783e0d7e
Akash Manohar J <akash@akash.im>
2014-02-28 08:26:06 +0530

    All-in-one C-file. Working tutorial.

--- c_src/stylish.c
+++ c_src/stylish.c
@@ -1,4 +1,8 @@
-/* complex.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef unsigned char byte;
 
 int foo(int x) {
   return x+1;
@@ -6,4 +10,25 @@ int foo(int x) {
 
 int bar(int y) {
   return y*2;
+}
+
+
+int main() {
+  int fn, arg, res;
+  byte buf[100];
+
+  while (read_cmd(buf) > 0) {
+    fn = buf[0];
+    arg = buf[1];
+    
+    if (fn == 1) {
+      res = foo(arg);
+    }
+    else if (fn == 2) {
+      res = bar(arg);
+    }
+
+    buf[0] = res;
+    write_cmd(buf, 1);
+  }
 }
--- ebin/stylish.app
+++
@@ -1,8 +0,0 @@
-{application,stylish,
-             [{description,[]},
-              {vsn,"1"},
-              {registered,[]},
-              {applications,[kernel,stdlib]},
-              {mod,{stylish_app,[]}},
-              {env,[]},
-              {modules,[complex1,complex5,hellhole,stylish_app,stylish_sup]}]}.
