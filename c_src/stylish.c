ba4c13543160b9a2a1a2b12eeb906f64a3191216
Akash Manohar J <akash@akash.im>
2014-02-28 08:38:07 +0530

    Switch C code to use erlang binary format

--- c_src/stylish.c
+++ c_src/stylish.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "erl_interface.h"
+#include "ei.h"
 
 typedef unsigned char byte;
 
@@ -14,21 +16,32 @@ int bar(int y) {
 
 
 int main() {
-  int fn, arg, res;
+  ETERM *tuplep, *intp;
+  ETERM *fnp, *argp;
+  int res;
   byte buf[100];
+  long allocated, freed;
+
+  erl_init(NULL, 0);
 
   while (read_cmd(buf) > 0) {
-    fn = buf[0];
-    arg = buf[1];
+    tuplep = erl_decode(buf);
+    fnp = erl_element(1, tuplep);
+    argp = erl_element(2, tuplep);
     
-    if (fn == 1) {
-      res = foo(arg);
-    }
-    else if (fn == 2) {
-      res = bar(arg);
+    if (strncmp(ERL_ATOM_PTR(fnp), "foo", 3) == 0) {
+      res = foo(ERL_INT_VALUE(argp));
+    } else if (strncmp(ERL_ATOM_PTR(fnp), "bar", 17) == 0) {
+      res = bar(ERL_INT_VALUE(argp));
     }
 
-    buf[0] = res;
-    write_cmd(buf, 1);
+    intp = erl_mk_int(res);
+    erl_encode(intp, buf);
+    write_cmd(buf, erl_term_len(intp));
+
+    erl_free_compound(tuplep);
+    erl_free_term(fnp);
+    erl_free_term(argp);
+    erl_free_term(intp);
   }
 }
